75af57bebda13a6911533d953fb4ec304e7ad36e
Eduardo Múcio <odraudemuciopereira@gmail.com>
2026-03-24 20:12:30 -0300

    add img light, discord

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toogleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/juliete.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/IMG_0093.jpg")
